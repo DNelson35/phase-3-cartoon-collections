a516d7b8166b6e4fc549f5b24f634c5ad27f693a
Damien Nelson <dhnisaman@gmail.com>
2023-05-07 14:57:30 -0400

    commit tree 9ac170f6169e96d95bdd520a9aed2e7b143e8302

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(arr)
+  arr.each.with_index {|i, index| puts "#{index + 1}. #{i}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map! {|i| i = "#{i.capitalize}!"}
+  arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  new_arr = arr.reject {|i| i.length <= 4}
+  new_arr.empty? ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find { |item| cheese_types.include?(item)}
 end
